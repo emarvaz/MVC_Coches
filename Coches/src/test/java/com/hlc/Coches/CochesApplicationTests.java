1ce0e63b26570b749b8f9c31c56d0d81618ea34e
Eduardo Martínez Vázquez <martinez.vazquez.eduardo@iescamas.es>
2025-01-09 08:47:40 +0100

    Primer commit

---
+++ Coches/src/test/java/com/hlc/Coches/CochesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlc.Coches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CochesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
